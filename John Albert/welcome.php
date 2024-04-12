59c1ea3392e8597890f7d42318467f511c35e065
John Albert <johnalbertd00@gmail.com>
2024-04-12 11:42:51 +0800

    John

---
+++ John Albert/welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Training</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+    <?php
+    echo '<h2>Hi, I am John</h2>';
+    // echo '<h3>Hi, I am John</h3>';
+    ?>
+</body>
+</html>
