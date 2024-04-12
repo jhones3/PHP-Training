c7cb7440604221a025bdba1c1b14f9ff35c2b437
Danniel Libor <dlibor.dev@gmail.com>
2024-04-12 11:21:55 +0800

    added welcome page

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome to PHP!</h1>
+
+    <?php
+    echo "Hello World!";
+    echo "<h2>PHP is fun!</h2>";
+
+    print "I'm about to learn PHP!";
+    print "<h2>PHP is easy!</h2>";
+    ?>
+</body>
+
+</html>
